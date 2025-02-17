b8f3c9c5a58ca76a3605e6e405ca7cff8dd1c9bd
Marsol4 <enzogmarsola@gmail.com>
2025-02-17 11:17:44 -0300

    adiciona o metodo de dsitribuir carta para o jogador

--- src/main/java/com/example/Jogo.java
+++ src/main/java/com/example/Jogo.java
@@ -4,4 +4,15 @@ public class Jogo {
     protected Monte monte = new Monte();
     protected Jogador jogador = new Jogador();
     protected Computador computador = new Computador();
+
+    public Jogo(){
+        monte.embaralhar();
+    }
+
+    public Carta distribuirCartaParaJogador(Jogador jogador){
+        var carta = monte.virar();
+        jogador.receberCarta(carta);
+
+        return carta;
+    }
 }
