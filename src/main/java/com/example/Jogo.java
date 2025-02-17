3f310a9a6324d302c194996c73917ec8cd5dd134
Marsol4 <enzogmarsola@gmail.com>
2025-02-17 11:07:39 -0300

    Cria a classe Jogo e adiciona seus atributos

---
+++ src/main/java/com/example/Jogo.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Jogo {
+    protected Monte monte = new Monte();
+    protected Jogador jogador = new Jogador();
+    protected Computador computador = new Computador();
+}
