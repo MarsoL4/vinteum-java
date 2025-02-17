2ec492441c193387b59ead77877f1af4375b31f0
Marsol4 <enzogmarsola@gmail.com>
2025-02-17 10:44:36 -0300

    cria um loop for each para a criação das cartas

--- src/main/java/com/example/Monte.java
+++ src/main/java/com/example/Monte.java
@@ -8,6 +8,9 @@ public class Monte {
 
     public Monte (){
         for (int numero = 0; numero <= 13; numero++) {
+            for (Carta carta : cartas) {
+                
+            }
             cartas.add(new Carta(numero, Naipe.Hearts));
             cartas.add(new Carta(numero, Naipe.Diamonds));
             cartas.add(new Carta(numero, Naipe.Clubs));
