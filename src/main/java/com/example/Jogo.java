e0a84b18225fe75fe0a88be0477b177522b5bce7
Marsol4 <enzogmarsola@gmail.com>
2025-02-17 11:30:39 -0300

    adiciona o metodo acabou responsavel por finalizar o jogo

--- src/main/java/com/example/Jogo.java
+++ src/main/java/com/example/Jogo.java
@@ -10,9 +10,29 @@ public class Jogo {
     }
 
     public Carta distribuirCartaParaJogador(Jogador jogador){
+        if (jogador.parou()){
+            return null;
+        }
+
         var carta = monte.virar();
         jogador.receberCarta(carta);
 
         return carta;
+    }
+
+    public boolean acabou(){
+        //alguém estourou
+        //ou
+        //os dois pararam
+    
+        if (jogador.getPontos() > 21 || computador.getPontos() > 21){
+            return true;
+        }
+
+        if (jogador.parou() && computador.parou()){
+            return true;
+        }
+
+        return false;
     }
 }
