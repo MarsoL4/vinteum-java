d7488db9d04162d993fbc3abe93e25d1101affdf
Marsol4 <enzogmarsola@gmail.com>
2025-02-17 10:55:53 -0300

    adiciona um metodo de embaralhar as cartas usando collections

--- src/main/java/com/example/Monte.java
+++ src/main/java/com/example/Monte.java
@@ -1,18 +1,23 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Monte {
     private List<Carta> cartas = new ArrayList<>();
 
     public Monte (){
+        //Criar 52 cartas 
         for (int numero = 0; numero <= 13; numero++) {
-            for (Naipe naipe : Naipe.values()) {
+            for (Naipe naipe : Naipe.values()) { //Se algum valor for adicionado aos naipes, não será necessário mexer aqui pois já irá puxar os valores atualizados
                 cartas.add(new Carta(numero, naipe));
-                
             }
 
         }
+    }
+
+    public void embaralhar(){
+        Collections.shuffle(cartas);
     }
 }
